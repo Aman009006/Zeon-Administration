57ff04a0f088a50e990a94a57b095318547ebff7
Israilov Sardor <Sardor_09062002@mail.ru>
2022-05-10 16:53:04 +0600

    updated code

--- src/Page/Chat/Chat.jsx
+++ src/Page/Chat/Chat.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Chat.scss";
 import fon from "../../assets/img/fon.jpg";
+import close from "../../assets/img/close.png";
 const Chat = () => {
+  const [zoom, setZoom] = useState(false);
+  const [zomimg, setZomImg] = useState(0);
   const [count, setCount] = useState(4);
+
+  const [message, setMessage] = useState("");
+  const [falid, setFalid] = useState(false);
+
+  useEffect(() => {
+    if (message) {
+      setFalid(false);
+    } else {
+      setFalid(true);
+    }
+  }, [message]);
+
   return (
     <section className="chat_wrapper">
       <div className="back_block">
         <button className="back_button">
-          <Link to="/" className="chat_link">
-            BACK
+          <Link to="/">
+          <div class="triangle-left"></div>
           </Link>
         </button>
-
         <button onClick={() => setCount(count + 1)} className="us_button">
           {count} <span>US</span>
+        </button>
+        <button className="back_button">
+          <Link to="/" >
+         <div class="triangle-right"></div>   
+          </Link>
         </button>
       </div>
       <div className="chat_container">
@@ -23,14 +42,7 @@ const Chat = () => {
             <p className="chat_text">GLOBAL CHAT</p>
           </div>
           <div className="chat_main">
-            <div className="chat_line">
-              <p className="ss">N</p>
-              <p className="ss">A</p>
-              <p className="ss">R</p>
-              <p className="ss">U</p>
-              <p className="ss">T</p>
-              <p className="ss">O</p>
-            </div>
+            <div className="chat_line"></div>
             <div className="chat_messenger">
               <div className="messenger">
                 <p className="user">User:</p>
@@ -38,10 +50,16 @@ const Chat = () => {
               </div>
               <div className="messenger">
                 <p className="user">User:</p>
-                <img src={fon} alt="" />
+                <img
+                  src={fon}
+                  alt=""
+                  onClick={() => {
+                    setZoom(true);
+                    // setZomImg(index);
+                  }}
+                />
               </div>
               <div className="messenger_me">
-                <p className="user">User:</p>
                 <p className="text">text</p>
               </div>
               <div className="messenger">
@@ -49,17 +67,10 @@ const Chat = () => {
                 <p className="text">text</p>
               </div>
               <div className="messenger_me">
-                <p className="user">User:</p>
                 <p className="text">text</p>
               </div>
             </div>
-            <div className="chat_line2">
-              <p>S</p>
-              <p>A</p>
-              <p>S</p>
-              <p>K</p>
-              <p>I</p>
-            </div>
+            <div className="chat_line2"></div>
           </div>
           <div className="chat_footer">
             <form className="input_block">
@@ -67,12 +78,17 @@ const Chat = () => {
                 type="text"
                 placeholder="Enter your message"
                 className="chat_input"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               />
               <div className="form_buttom">
-                <button type="submit" className="input_button">
+                <button
+                  disabled={falid}
+                  type="submit"
+                  className={falid ? "input_button" : "input__di"}
+                >
                   SEND
                 </button>
-                {/* <button className="input_clip"><img src={bul} alt="" /></button> */}
               </div>
             </form>
           </div>
@@ -143,6 +159,14 @@ const Chat = () => {
           </div>
         </div>
       </div>
+      {zoom ? (
+        <div className="modal_img-zoom">
+          <div onClick={() => setZoom(false)} className="close">
+            <img src={close} alt="" />
+          </div>
+          <img src={fon} alt="" className="modal_img_img" />
+        </div>
+      ) : null}
     </section>
   );
 };
